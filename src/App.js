90c7069f7f25e76719fc033899936491ce277922
k_o!! <117063128+koukikubo@users.noreply.github.com>
2024-07-25 11:51:06 +0900

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import TodoList from "./TodoList";
 
 function App() {
+  const [todos, setTodos] = useState([
+    { id: 1, name: "Todo1", completed: false },
+  ]);
+
+  const handleAddTodo = () => {
+    //タスクを追加する。
+    
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <TodoList todos={todos} />
+      <input type="text" />
+      <button Onclick={handleAddTodo}>タスクを追加</button>
+      <button>完了したタスクを削除</button>
+      <div>残りのタスク:0</div>
     </div>
   );
 }
